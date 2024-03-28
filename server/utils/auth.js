24589c9334d19206c36e600112113cd2e5f21017
JB <bradburyjesse7@gmail.com>
2024-03-28 11:01:02 -0700

    updated auth.js file for graphQl

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,8 @@
+// Converting this auth file to work with our graphQl API.
+// I've updated the error sections, the authMiddleware function has been changed to only take in the req, then they have authorization in their headers then we take that token from the request. 
+// We us jwt to verrify the token, then return the 
+
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,18 +10,30 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // Adding authentication error for graphQL
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+
+  // // function for our authenticated routes
+  // authMiddleware: function (req, res, next) {
+  //   // allows token to be sent via  req.query or headers
+  //   let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // if (!token) {
+    //   return res.status(400).json({ message: 'You have no token!' });
+    // }
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +42,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
+    // next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
