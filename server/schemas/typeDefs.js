0f38b54450a8e9dd54e5709e782256b9c533b7d0
JB <bradburyjesse7@gmail.com>
2024-03-28 22:07:36 -0700

    Updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,3 @@
-var BookData = new GraphQLInputObjectType({
-  name: "BookData", 
-  fields: {
-    authors: [], 
-    description: { type: new GraphQLNonNull(GraphQLString) },
-    title: { type: new GraphQLNonNull(GraphQLString) },
-    bookId: { type: new GraphQLNonNull(GraphQLString) },
-    image: { type: new GraphQLNonNull(GraphQLString) },
-    link: { type: new GraphQLNonNull(GraphQLString) },
-  }
-
-
-})
-
 const typeDefs = `
   type User {
     _id: ID
@@ -29,6 +15,14 @@ const typeDefs = `
     image: String
     link: String
   }
+  input BookInput {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String!
+    link: String!
+  }
 
   type Auth {
     token: ID!
@@ -42,7 +36,7 @@ const typeDefs = `
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(book: BookData!): User
+    saveBook(book: BookInput!): User
     removeBook(bookId: ID!): User
   }
 
